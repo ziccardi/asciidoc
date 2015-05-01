05180165032d02f0fc1aeac94ac7852862223f40
ziccardi <massimiliano.ziccardi@gmail.com>
2015-05-01 12:24:08 +0100

    First commit

---
+++ asciidoc-multimodule/module1/src/main/java/com/test/module1/MyTestClass1.java
@@ -0,0 +1,11 @@
+package com.test.module1;
+
+public class MyTestClass1 {
+
+	// tag::myTag1[]
+	public void testMethod1() {
+		System.out.println ("method 1");
+	}
+	// end::myTag1[]
+	
+}
---
+++ asciidoc-multimodule/module2/src/main/java/com/test/module2/MyTestClass2.java
@@ -0,0 +1,10 @@
+package com.test.module2;
+
+public class MyTestClass2 {
+
+	// tag::myTag2[]
+		public void testMethod2() {
+			System.out.println ("method 2");
+		}
+		// end::myTag2[]
+}
